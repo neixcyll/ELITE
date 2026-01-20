b9775f212fd59f3549d3369f11402880c5220103
evalunaputrijanur <evalunaputrijanur76@gmail.com>
2026-01-20 21:17:25 +0700

    judulbg-struktur

--- script.js
+++ script.js
@@ -132,11 +132,46 @@ const cardData = {
     content: `
       <h2>Pengurus Inti ELITE</h2>
       <ul>
-        <li><span class="jabatan">Ketua:</span> Nama Ketua</li>
-        <li><span class="jabatan">Wakil Ketua:</span> Nama Wakil</li>
-        <li><span class="jabatan">Sekretaris:</span> Nama Sekretaris</li>
-        <li><span class="jabatan">Bendahara:</span> Nama Bendahara</li>
-        <li><span class="jabatan">Koordinator Divisi:</span> Nama Koordinator</li>
+        <li><span class="jabatan">Pembina:</span> -</li>
+        <li><span class="jabatan">Pembimbing:</span> Rezal Pramudiastama</li>
+        <li><span class="jabatan">Ketua:</span> Bayu Romadhon Saputra - XII TEI</li>
+        <li>
+          <span class="jabatan">Sekretaris:</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Moch. Raihan Aufa Rizki Munir - XII RPL 2</li>
+            <li>Abhista Nareswari Sugiyanto - XI RPL 2</li>
+            <li>Mahmud Azzam - XI TAV 2</li>
+            <li>Neil Sawabul Jazil - XI RPL 2</li>
+          </ol>
+        </li>
+        <li>
+          <span class="jabatan">Bendahara:</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Berliana Fitri Aulia - XI RPL 2</li>
+            <li>Eva Luna Putri Janur - XI RPL 2</li>
+            <li>Muhammad Haykal Ubaidilhaq - XI TEI</li>
+            <li>Muhammad Ibnu Ridho - XI TEI</li>
+          </ol>
+        </li>
+        <li>
+          <span class="jabatan">Inventaris:</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Antananda Reksadjendra Dimasing Bagavagita - XII TAV 2</li>
+            <li>Radittya Vitsava Darma - XI RPL 2</li>
+            <li>Satya Novario Rizky Putranto - XI RPL 2</li>
+            <li>Satrio Dwi Syairendra - XI RPL 2</li>
+            <li>Wildana Wargadinata - XI TEI</li>
+          </ol>
+        </li>
+        <li>
+          <span class="jabatan">Kominfo:</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Abelya Octaviani - XII RPL 3</li>
+            <li>Andika Anwar Bawana - XI TEI</li>
+            <li>Mey Adinda Luckytasari - XI TAV 2</li>
+            <li>Tunggul Catra Wisesa - XI TEI</li>
+          </ol>
+        </li>
       </ul>
     `
   },
@@ -145,11 +180,49 @@ const cardData = {
     content: `
       <h2>Divisi Riset ELITE</h2>
       <ul>
-        <li><span class="jabatan">Divisi Soccer Robot:</span> Fokus pada pengembangan robot sepak bola</li>
-        <li><span class="jabatan">Divisi Line Follower:</span> Riset robot line tracking</li>
-        <li><span class="jabatan">Divisi Fire Fighting:</span> Robot pemadam kebakaran</li>
-        <li><span class="jabatan">Divisi Programming:</span> Pengembangan software dan AI</li>
-        <li><span class="jabatan">Divisi Elektronika:</span> Desain PCB dan hardware</li>
+        <li>
+          <span class="jabatan">Robot Soccer</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Antananda Reksadjendra Dimasing Bagavagita - XII TAV 2</li>
+            <li>Berliana Fitri Aulia - XI RPL 2</li>
+            <li>Radittya Vitsava Darma - XI RPL 2</li>
+          </ol>
+        </li>
+        <li>
+          <span class="jabatan">Robot Sumo</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Moch. Raihan Aufa Rizki Munir - XII RPL 2</li>
+            <li>Muhammad Haykal Ubaidilhaq - XI TEI</li>
+            <li>Wildana Wargadinata - XI TEI</li>
+          </ol>
+        </li>
+        <li>
+          <span class="jabatan">Robot Transporter</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Bayu Romadhon Saputra - XII TEI</li>
+            <li>Mey Adinda Luckytasari - XI TAV 2</li>
+            <li>Tunggul Catra Wisesa - XI TEI</li>
+          </ol>
+        </li>
+        <li>
+          <span class="jabatan">Robot LTM</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Abhista Nareswari Sugiyanto - XI RPL 2</li>
+            <li>Andika Anwar Bawana - XI TEI</li>
+            <li>Neil Sawabul Jazil - XI RPL 2</li>
+            <li>Satya Novario Rizky Putranto - XI RPL 2</li>
+            <li>Satrio Dwi Syairendra - XI RPL 2</li>
+          </ol>
+        </li>
+        <li>
+          <span class="jabatan">Karya Tulis Ilmiah</span>
+          <ol style="margin-top: 8px; padding-left: 20px;">
+            <li>Abelya Octaviani - XII RPL 3</li>
+            <li>Eva Luna Putri Janur - XI RPL 2</li>
+            <li>Mahmud Azzam - XI TAV 2</li>
+            <li>Muhammad Ibnu Ridho - XI TEI</li>
+          </ol>
+        </li>
       </ul>
     `
   }
